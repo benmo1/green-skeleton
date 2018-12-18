67c3339f1dba51cd9e2dde2d11fe1c9538333a94
Ben Morris <benjamin.morris77@gmail.com>
2018-12-18 20:23:31 +0000

    Add +ssh function first draft
    This could be more succinct

---
+++ components/ssh_aliases.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+config_file=~/.ssh/config
+
+function +ssh () {
+    if ! [[ -f $config_file ]] ; then
+        echo "$config_file not found!"
+        return
+    fi
+
+    hosts=`egrep '^\s*\bHost\b\s+(\w+)\s*$' $config_file | awk '{$1=$1}1' | cut -d' ' -f2 | grep ${1-''}`
+
+    if [[ -z $hosts ]] ; then
+        echo "No hosts in $config_file!"
+        return
+    fi
+
+    echo 'Where would you like to go?'
+    select h in $hosts; do
+        if [ -n "$h" ] ; then
+            break
+        fi
+    done
+
+    ssh "$h"
+}
